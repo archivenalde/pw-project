1a28dfaa8e4693bdfa7e21b9ba1aa685dbcbb955
Adlane LADJAL <ladjal.adlane@gmail.com>
2018-09-23 12:10:05 +0200

    Possibilite d'ajouter juste un produit, pas ses composants

--- ajout_produit.php
+++ ajout_produit.php
@@ -1,14 +1,17 @@
 <?php
 
 try {
-	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+	$bdd = new PDO('mysql:host=localhost;dbname=pw-project;charset=utf8', 'root', '');
 } catch (Exception $e) {
     die('Erreur : '.$e->getMessage());
 	
 }
 
-$req = $bdd->prepare('INSERT INTO produits(nom, categorie, date_ajout) VALUES (:nom, categorietest, CURDATE())');
+$bdd->exec('INSERT INTO Produits(nom, categorie, date_ajout) VALUES (\'testphpexec\', \'categorietest\', \'2018-09-03\');');
+
+$req = $bdd->prepare('INSERT INTO Produits(nom, categorie, date_ajout) VALUES (:nom, \'categorietest\', :date_ajout)');
 $req->execute(array(
-	'nom' => $_POST["product-name"]
+	'nom' => $_POST["product-name"],
+	'date_ajout' => date("Y-m-d")
 	));
 ?>
